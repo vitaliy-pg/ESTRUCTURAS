b872818a1e2f6339a0668bfe2f1a803981649b29
vitaliy-pg <aljndrvt@myuax.com>
2023-12-12 13:18:22 +0100

    literalmente pongo lo que copilot me ha dicho xd

--- Estructuras y funciones.cpp
+++ Estructuras y funciones.cpp
@@ -54,3 +54,22 @@ private:
 };
 
 int main() {
+    // Crear una instancia de la estructura Estudiante
+    Estudiante estudiante1 = {"Juan Perez", 20, 85.5};
+
+    // a. Pasar una estructura por valor como argumento
+    imprimirEstudiantePorValor(estudiante1);
+
+    // b. Pasar una estructura por referencia como argumento
+    imprimirEstudiantePorReferencia(estudiante1);
+
+    // c. Pasar una estructura por dirección como argumento
+    imprimirEstudiantePorDireccion(&estudiante1);
+
+    // d. De la programación funcional a la programación orientada a objetos
+    ListaEstudiantes listaEstudiantes;
+    listaEstudiantes.agregarEstudiante(estudiante1);
+    listaEstudiantes.imprimirListaEstudiantes();
+
+    return 0;
+}
