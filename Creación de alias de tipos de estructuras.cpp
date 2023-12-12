5f6b5516ac8ee3ee3850f7e2ca4abef17279a81e
vitaliy-pg <aljndrvt@myuax.com>
2023-12-12 13:07:58 +0100

    imprimo la informacion del estudiante

--- Creación de alias de tipos de estructuras.cpp
+++ Creación de alias de tipos de estructuras.cpp
@@ -11,3 +11,11 @@ typedef Estudiante MiEstudiante;
 int main() {
     // Crear una instancia del alias de la estructura Estudiante
     MiEstudiante miEstudiante = {"Juan Perez", 20, 85.5};
+
+    std::cout << "Estudiante:" << std::endl;
+    std::cout << "Nombre: " << miEstudiante.nombre << std::endl;
+    std::cout << "Edad: " << miEstudiante.edad << " años" << std::endl;
+    std::cout << "Promedio: " << miEstudiante.promedio << std::endl;
+
+    return 0;
+}
