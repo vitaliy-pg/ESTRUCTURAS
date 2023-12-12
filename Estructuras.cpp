4b12a00e3bc0b5c09a9423df69c312d5024f8658
vitaliy-pg <aljndrvt@myuax.com>
2023-12-12 12:06:31 +0100

    funcion para agregar estudiantes

--- Estructuras.cpp
+++ Estructuras.cpp
@@ -5,17 +5,7 @@ struct Estudiante {
     int edad;
     float promedio;
 };
-int main() {
-    // Crear una instancia de la estructura Estudiante
-    Estudiante estudiante1;
-    estudiante1.nombre = "Juan Perez";
-    estudiante1.edad = 20;
-    estudiante1.promedio = 85.5;
-
-    // Imprimir la información del estudiante
-    std::cout << "Nombre: " << estudiante1.nombre << std::endl;
-    std::cout << "Edad: " << estudiante1.edad << " años" << std::endl;
-    std::cout << "Promedio: " << estudiante1.promedio << std::endl;
-
-    return 0;
+void agregarEstudiante(std::vector<Estudiante>& listaEstudiantes, const Estudiante& nuevoEstudiante) {
+    listaEstudiantes.push_back(nuevoEstudiante);
 }
+
