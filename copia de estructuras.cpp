3f96efada43b99820f641eeee2d733abf7fd96e6
vitaliy-pg <aljndrvt@myuax.com>
2023-12-12 12:28:16 +0100

    funcion para copiar estructura

--- copia de estructuras.cpp
+++ copia de estructuras.cpp
@@ -5,3 +5,5 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+Estudiante copiarEstudiante(const Estudiante& original) {
+    Estudiante copia;  // Se crea una nueva instancia de Estudiante
