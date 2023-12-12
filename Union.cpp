f213af0828a523709b65f6d646d5c9ad0362446d
vitaliy-pg <aljndrvt@myuax.com>
2023-12-12 12:22:48 +0100

    asigno una cadena

--- Union.cpp
+++ Union.cpp
@@ -14,3 +14,9 @@ std::cout << "Valor entero: " << miVariable.entero << std::endl;
 
 miVariable.flotante = 3.14f;
 std::cout << "Valor flotante: " << miVariable.flotante << std::endl;
+
+miVariable.cadena = "Hola, mundo!";
+std::cout << "Valor cadena: " << miVariable.cadena << std::endl;
+
+return 0;
+}
