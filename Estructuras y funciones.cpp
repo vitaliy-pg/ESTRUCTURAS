a4314936b1febc7689de78bc3464507afbe5cfa6
vitaliy-pg <aljndrvt@myuax.com>
2023-12-12 13:14:02 +0100

    Paso una estructura por valor como argumento

--- Estructuras y funciones.cpp
+++ Estructuras y funciones.cpp
@@ -7,3 +7,11 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+// a. Pasar una estructura por valor como argumento
+void imprimirEstudiantePorValor(Estudiante estudiante) {
+    std::cout << "Estudiante (por valor):" << std::endl;
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << " años" << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
